8a489ef9edacb9bc2a8c328bc540ab4803f172bd
chichichichige <912747904@qq.com>
2024-02-04 15:08:24 +0800

    添加 RSS 订阅源

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://gleeful-belekoy-e3ae83.netlify.app/",
+  integrations: [preact()]
+});
